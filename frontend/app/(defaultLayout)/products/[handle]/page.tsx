5d7785dcb3e48e14b428694992cf2f64a950527a
Massimo De Marchi <massimo@inkofpixel.com>
2023-12-21 15:19:47 +0100

    wip

--- frontend/app/(defaultLayout)/products/[handle]/page.tsx
+++ frontend/app/(defaultLayout)/products/[handle]/page.tsx
@@ -1,6 +1,7 @@
 import { CACHE_DISABLED, DEFAULT_STORE_CODE } from '@config/env';
 import Product from '@pages/api/nextjs/cache/product';
 import { ProductTemplate } from '@templates/product';
+import type { Metadata } from 'next';
 import { notFound, redirect } from 'next/navigation';
 
 interface ProductPageProps {
@@ -9,6 +10,31 @@ interface ProductPageProps {
    };
    searchParams: {
       disableCacheGets?: string | string[] | undefined;
+   };
+}
+
+export async function generateMetadata({
+   params,
+   searchParams,
+}: ProductPageProps): Promise<Metadata> {
+   const skipCache = shouldSkipCache(searchParams);
+   const product = await Product.get(
+      { handle: params.handle, storeCode: DEFAULT_STORE_CODE },
+      { forceMiss: skipCache }
+   );
+
+   if (product?.__typename !== 'Product') return {};
+
+   const proOnly =
+      product?.tags.find((tag: string) => tag === 'Pro Only') != null;
+
+   return {
+      robots: proOnly
+         ? {
+              follow: false,
+              index: false,
+           }
+         : undefined,
    };
 }
 
